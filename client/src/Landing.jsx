584181a6fca490cf5ffcb7aabffc4b46a2b3040e
1234gk <144155567+1234gk@users.noreply.github.com>
2024-10-19 09:20:11 +0530

    adding client and server

---
+++ client/src/Landing.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function Landing(){
+return(
+    <>
+    <div>
+avs
+    </div>
+    </>
+)
+}
+export default Landing;
---
+++ server/src/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+import { useNavigate } from 'react-router-dom';
+import './App.css'
+
+function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+  const [error, setError] = useState("")
+
+  async function Log(e) {
+    e.preventDefault();
+
+     
+    const resp = await fetch("http://localhost:5172/",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+    if (resp.ok) {
+    
+      navigate('/landing'); 
+    } else {
+      
+   setError("invalid user")
+    }
+
+ 
+  }
+
+   
+  
+
+  return (
+    <>
+      <div>
+        <fieldset>
+        <h1>Login</h1><br />
+        <form >
+          <label htmlFor="email">Email</label><br />
+          <input 
+            type="text" 
+            name="email" 
+            onChange={(e) => setEmail(e.target.value)} 
+             
+          required/><br />
+          <p>{error}</p>
+          
+          <label htmlFor="password">Password</label><br />
+          <input 
+            type="password" 
+            name="password" 
+            onChange={(e) => setPassword(e.target.value)} /><br />
+       <div className="check"><input type="checkbox" /><span>remember me</span></div>
+          <br />
+     <center> <button type='submit' onClick={Log}>login</button> </center>   
+        </form><br />
+        <p> do not have any account <a href="/register">register</a>
+     </p>
+        </fieldset><br />
+    
+
+      </div>
+    </>
+  )
+}
+
+export default Login;
+
